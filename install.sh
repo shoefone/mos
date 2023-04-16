6763b06816f87451b7223b55931ea57a0b652fc0
shoefone <52260452+shoefone@users.noreply.github.com>
2023-04-16 12:40:47 -0300

    Install some basic programs

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
+# This shell script is designed to be run on a newly-imaged raspberry pi computer
+# Base is: Raspberry Pi 4B with Raspberry Pi OS (Debian v11 Bullseye / systemd)
+
+# It is designed to be run as root
+
 # Set some global variables (always a good start!)
 localUser=username
 localHost=hostname
+
+# Install some programs
+apt-get install emacs
+apt-get install screen
+apt-get install firefox-esr
