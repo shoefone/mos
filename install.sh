fbffe2c036d3d1da2b67bdde7d6eed501dddcadf
shoefone <52260452+shoefone@users.noreply.github.com>
2023-06-27 00:16:15 -0300

    Added bash shebang line

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # This shell script is designed to be run on a newly-imaged raspberry pi computer
 # Base is: Raspberry Pi 4B with Raspberry Pi OS (Debian v11 Bullseye / systemd)
 
@@ -7,10 +8,28 @@
 localUser=username
 localHost=hostname
 
+# Bring Apt up-to-date
+echo "***************************** General ******************************************"
+apt-get -y update
+apt-get -y upgrade
+echo "Installing dialog..." # GUI for shellscript
+apt-get -y install dialog
+
+# Get the userinput needed for processing
+echo "***************************** Userinput ****************************************"
+
 # Choices here should follow the terminology used by Alsa
 # Common values are 'Headphones' (3.5mm jack), 'vc4hdmi0' (HDMI0 port), and 'vc4hdmi1' (HDMI1 port)
-# Get Alsa device list with $aplay -l
+
 # Get shairport-sync device list with shairport-sync -h
+
+# Get Alsa device list with $aplay -l
+#readarray -t alsaDevices <<<$(aplay -l | grep -Po 'card \d: \K\w*')
+
+# Prompt to choose music audio out
+#dialog --radiolist "Audio output for music:" 0 0 0 \
+# Prompt to choose video audio out
+
 kodiAudioDevice=hw:vc4hdmi0
 shairportAudioDevice=hw:Headphones
 bluetoothAudioDevice=hw:Headphones
@@ -18,20 +37,15 @@ owntoneAudioDevice=hw:Headphones
 
 #kodiVideoDevice
 
-# Bring Apt up-to-date
-echo "***************************** General ******************************************"
-apt-get -y update
-apt-get -y upgrade
-
 # Install some programs
 echo "***************************** Programs *****************************************"
-echo "Installing emacs..."
+echo "Installing emacs..." # Generally useful
 apt-get -y install emacs
-echo "Installing screen..."
+echo "Installing screen..." # Generally useful
 apt-get -y install screen
-echo "Installing firefox..."
+echo "Installing firefox..." # If any GUI webwork needs to be done
 apt-get -y install firefox-esr
-echo "Installing Samba..."
+echo "Installing Samba..." # Media sharing (NAS functionality)
 apt-get -y install samba samba-common-bin
 echo "Installing build software (for shairport-sync)..."
 apt-get -y install --no-install-recommends \
